ab10ecc3d57e973c30eb0dedd6588cf87c9ce99f
Gary Pamparà <gpampara@gmail.com>
2019-12-15 08:14:55 +0200

    Syncing the config - cleanup is still needed

--- early-init.el
+++ early-init.el
@@ -1,2 +1,8 @@
 ;; For Emacs 27 and above
 (setq package-enable-at-startup nil)
+
+
+;; Prevent the glimpse of un-styled Emacs by setting these early.
+(add-to-list 'default-frame-alist '(tool-bar-lines . 0))
+(add-to-list 'default-frame-alist '(menu-bar-lines . 0))
+(add-to-list 'default-frame-alist '(vertical-scroll-bars))
