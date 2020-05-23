91f7f621f966ad886c3fca222903db57f27e4ad0
Gary Pamparà <gpampara@gmail.com>
2020-05-23 10:20:13 +0200

    Cleanup and additional packages
    Added:
    - avy
    - disable-mouse
    - hydra
    - org-roam

--- early-init.el
+++ early-init.el
@@ -1,6 +1,9 @@
 ;; For Emacs 27 and above
 (setq package-enable-at-startup nil)
 
+;(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
 ;; Prevent the glimpse of un-styled Emacs by setting these early.
 (add-to-list 'default-frame-alist '(tool-bar-lines . 0))
