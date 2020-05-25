69ab5f93610820b19a85be78d1e0fcec7f35e62e
Gary Pamparà <gpampara@gmail.com>
2020-05-25 20:17:51 +0200

    Transition to use general.el for keybindings and total cleanup

--- init.el
+++ init.el
@@ -1,2 +1,15 @@
 (package-initialize)
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(general yasnippet-snippets yaml-mode writeroom-mode writegood-mode which-key web-mode use-package-ensure-system-package smart-hungry-delete scala-mode sbt-mode ripgrep rg request-deferred rainbow-mode rainbow-delimiters pug-mode projectile prettier-js popup-kill-ring poly-R persist pdf-tools outline-magic osx-dictionary org-noter org-bullets org-alert nix-mode mustache-mode modus-vivendi-theme magit-todos lsp-ui ledger-mode langtool json-mode js2-mode ivy-rich hydra hungry-delete haskell-mode gruvbox-theme god-mode flycheck-ledger flycheck-elm exec-path-from-shell ess elm-mode ebib dumb-jump doom-themes doom-modeline disable-mouse direnv dhall-mode dashboard csv-mode crux counsel company-terraform company-org-roam company-lsp coffee-mode boon auto-package-update auctex amx ag add-node-modules-path ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
