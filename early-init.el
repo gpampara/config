618fa01454628146d0c57072abdbde1831365e92
Gary Pamparà <gpampara@gmail.com>
2020-10-28 23:10:00 +0200

    Drop ivy/swiper/counsel for selectrum/prescient/ctrlf

--- early-init.el
+++ early-init.el
@@ -1,3 +1,11 @@
+;;; For performance
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+
+(add-hook 'after-init-hook #'(lambda ()
+                               ;; restore after startup
+                               (setq gc-cons-threshold 800000)))
+
 (setq package-enable-at-startup nil)
 
 (setq inhibit-startup-screen t)  ; don't display the help screen
--- init.el
+++ init.el
@@ -1,2 +1,15 @@
 (package-initialize)
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/org/thesis-todo.org" "~/org/gtd.org" "~/org/projects.org" "~/org/work.org" "~/org/calendar.org" "~/org/call.org" "~/org/birthdays.org" "/Users/gpampara/org/roam/2020-10-01.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
