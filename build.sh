7a3131781b3667fc844784745c084fea5adb045d
Gary Pamparà <gpampara@gmail.com>
2021-06-22 10:48:06 +0200

    Ensure that there are more file descriptors for macOS

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ulimit -S -n 2048  # Temporarily increase the number of file descriptors
+
 ## Taken from:
 ## https://www.reddit.com/r/NixOS/comments/iogoox/homemanager_with_flakes_on_non_nixos_system/
 
