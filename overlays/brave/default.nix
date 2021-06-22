4d10f559adbc6ca1f2154dece9f76aea90ac4f66
Gary Pamparà <gpampara@gmail.com>
2021-06-22 14:40:24 +0200

    Update brave version

--- overlays/brave/default.nix
+++ overlays/brave/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "brave";
-  version = "1.25.70";
+  version = "1.25.73";
 
   buildInputs = [ undmg ];
   sourceRoot = ".";
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     name = "Brave-Browser-x64-v${version}.dmg";
     url = "https://github.com/brave/brave-browser/releases/download/v${version}/Brave-Browser-x64.dmg";
-    sha256 = "lEhnSFn+c3UsVp9wp+MuhxOVgIAQdSFZyoLjj/z1kjA=";
+    sha256 = "SO4Jb7M2D8a7Iyi4PCRu2ONI+1mH6PMBuhXfwxlhmj0=";
   };
 
   meta = with pkgs.lib; {
