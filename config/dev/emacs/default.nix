0cb596746dc097f2a890593a806d5ce5e76a9fe7
Gary Pamparà <gpampara@gmail.com>
2021-10-13 06:58:05 +0200

    Try out project.el instead of projectile for a bit

--- config/dev/emacs/default.nix
+++ config/dev/emacs/default.nix
@@ -134,7 +134,7 @@
             :preview-key (kbd "M-."))
 
           ;; Specify that searches with consult should start from the root of the project (if exists)
-          (setq consult-project-root-function #'projectile-project-root)
+          (setq consult-project-root-function #'project-current)
 
           ;; Use smart-casing for ripgrep (i.e. case-insensitive search until uppercase character is provided)
           (setq consult-ripgrep-command "rg -S --null --line-buffered --color=ansi --max-columns=1000 --no-heading --line-number . -e ARG OPTS")
@@ -166,6 +166,8 @@
         config = ''
           (setq dashboard-week-agenda t)
 
+          (setq dashboard-projects-backend 'project-el)
+
           ;; TODO: add the agenda for the current day that is more flexible than the default implementation in dashboard.el
           ;; The filter should take all agenda items for the current day and display them (including birthdays)
           (setq dashboard-items '((agenda . 10)
@@ -179,6 +181,7 @@
 
           ;; Show buttons
           (setq dashboard-set-navigator t)
+
           ;; Format: "(icon title help action face prefix suffix)"
           (setq dashboard-navigator-buttons
                 `(;; line1
@@ -822,7 +825,7 @@
       };
 
       projectile = {
-        enable = true;
+        enable = false;
         diminish = [ "projectile-mode" ];
         command = [ "projectile-mode" "projectile-project-root" ];
         bindKeyMap = {
