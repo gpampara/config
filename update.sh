535c367ab8b58f1a082065b5db2b4d559dba100b
Gary Pamparà <gpampara@gmail.com>
2021-10-17 08:58:29 +0200

    Result is a string, not an error signal

--- update.sh
+++ update.sh
@@ -2,7 +2,9 @@
 
 SELECTED=$(nix flake metadata --json --quiet | jq -r '.locks.nodes.root.inputs | keys | . + ["all"] | .[]' | fzf)
 
-if [ "$SELECTED" == "all" ]; then
+if [ -z "$SELECTED" ]; then
+    exit 1;
+elif [ "$SELECTED" == "all" ]; then
     nix flake update
 else
     nix flake lock --update-input "$SELECTED"
