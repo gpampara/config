371bee6da965ba6875626a434a2827a3ca39ae4a
Gary Pamparà <gpampara@gmail.com>
2021-11-11 06:53:50 +0200

    Update flake.lock and patch vagrant 2.2.19 for monterey

--- overlays/default.nix
+++ overlays/default.nix
@@ -19,4 +19,20 @@ final: prev: {
     patches = [ ./unset-is-macho.patch ];
   };
 
+  vagrant = prev.vagrant.overrideAttrs(oldAttrs: {
+    postInstall =
+      let
+        # https://github.com/hashicorp/vagrant/issues/12583
+        replacement = prev.fetchurl {
+          url = "https://raw.githubusercontent.com/hashicorp/vagrant/42db2569e32a69e604634462b633bb14ca20709a/plugins/hosts/darwin/cap/path.rb";
+          sha256 = "2YKz694IHzFgN0yUhIgMtwZDUGgTPhhWBC78tE7fV/k=";
+        };
+      in
+      ''
+        ${oldAttrs.postInstall}
+
+        cp ${replacement} $out/lib/ruby/gems/2.7.0/gems/vagrant-2.2.19/plugins/hosts/darwin/cap/path.rb
+      '';
+  });
+
 }
