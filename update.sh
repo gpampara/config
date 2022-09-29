2231bc15b4fc77c08334a92803a9a2f31f9f420b
Gary Pamparà <gpampara@gmail.com>
2022-09-29 06:25:01 +0200

    Improve Brave version update

--- update.sh
+++ update.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 function update_brave_version () {
+    CURRENT_VERSION=$(nix eval --impure --raw --expr "(import ./overlays/brave/brave-version.info).version")
     CURRENT_HASH=$(nix eval --impure --raw --expr "(import ./overlays/brave/brave-version.info).sha256")
     VERSION=$(curl -sL https://brave-browser-apt-release.s3.brave.com/dists/stable/main/binary-amd64/Packages | sed -r -n 's/^Version: (.*)/\1/p' | head -n1)
-    HASH=$(nix-prefetch-url "https://github.com/brave/brave-browser/releases/download/v${VERSION}/Brave-Browser-x64.dmg" "${CURRENT_HASH}" --type sha256 2>/dev/null)
-    echo "{ version = \"${VERSION}\"; sha256 = \"${HASH}\"; }" > ./overlays/brave/brave-version.info
+
+    if [ "${CURRENT_VERSION}" != "${VERSION}" ]; then
+        echo "New version of Brave found, updating metadata..."
+        HASH=$(nix-prefetch-url "https://github.com/brave/brave-browser/releases/download/v${VERSION}/Brave-Browser-x64.dmg" "${CURRENT_HASH}" --type sha256 2>/dev/null)
+        echo "{ version = \"${VERSION}\"; sha256 = \"${HASH}\"; }" > ./overlays/brave/brave-version.info
+    fi
 }
 
 SELECTED=$(nix flake metadata --json --quiet | jq -r '.locks.nodes.root.inputs | keys | . + ["all", "brave"] | .[]' | fzf)
